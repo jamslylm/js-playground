9cd923d40e423579038a6528c3addc67ac9c3088
Jamsly-love MINE <mjamsly98@gmail.com>
2021-11-15 11:31:31 -0500

    Work with promise

--- 8-promises.js
+++ 8-promises.js
@@ -1,3 +1,4 @@
+/*
 const doWorkPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         reject('This is an error!')
@@ -9,4 +10,36 @@ doWorkPromise.then((result) => {
     console.log('Success!', result)
 }).catch((error) => {
     console.log('Error!', error)
+})*/
+
+// Working promise
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// Without promise chaining
+/*
+add(1, 9).then((sum) => {
+    console.log(sum)
+    add(sum, 8).then((result) => {
+        console.log(result)
+    }).catch((er) => {
+        console.log(er)
+    })
+}).catch((e) => {
+    console.log(e)
+})*/
+
+// With promise chaining
+add(5, 8).then((sum) => {
+    console.log(sum)
+    return add(sum, 5)
+}).then((result) => {
+    console.log(result)
+}).catch((er) => {
+    console.log(er)
 })
